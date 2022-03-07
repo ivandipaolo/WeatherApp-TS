e5ce99151124c8f73e84e73169ba1f4870ec8d21
Ivan Ari Di Paolo <ivandipa8@gmail.com>
2022-03-07 19:24:34 +0100

    Added geolocation permission

--- src/components/Welcome/Welcome.tsx
+++ src/components/Welcome/Welcome.tsx
@@ -1,58 +1,60 @@
 import { useState, useEffect } from 'react';
-import { StyledCurrentWeather } from '../CurrentWeather/StyledCurrentWeather';
 import { useSelector, useDispatch } from 'react-redux';
 import dayjs from 'dayjs';
 import { RootState } from '../../redux/store/store';
+
+import { StyledMessage, StyledWelcome } from './StyledWelcome';
 import { WeatherIcon } from '../WeatherIcon';
 import { kelvinToCelcius } from '../../helpers/unitConvertion';
 import { getWeather } from '../../helpers/weatherSearcher';
 import { setCurrentWeather } from '../../redux/actions/weatherActions';
 
+import locationIcon from '../../assets/locationIcon.png';
+
 export const Welcome = () => {
-    const [geolocation, setGeolocation] = useState<boolean>(false)
+    const [location, setLocation] = useState<boolean>()
+    const [error, setError] = useState<string>()
+
     const { current } = useSelector((state: RootState) => state.WeatherInterface)
-    const { selectedPlace } = useSelector((state: RootState) => state.placeInterface)
     const { dt, feelsLike, temp, main, id, icon } = current;
     const date = dayjs(new Date(dt * 1000))
 
     const dispatch = useDispatch();
 
+    const handleSuccess = async (position: { coords: { latitude: any; longitude: any; }; }) => {
+        const { latitude, longitude } = position.coords;
+        setLocation(true);
+        const weather = await getWeather(latitude, longitude);
+        if (weather) {
+            dispatch(setCurrentWeather(weather.current));
+        }
+    };
+
+    const handleError = (error: { message: any; }) => {
+        setError(error.message);
+    };
+
     useEffect(() => {
         if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(async function (position) {
-                setGeolocation(true);
-                const weather = await getWeather(position.coords.latitude, position.coords.longitude);
-                if (weather) {
-                    dispatch(setCurrentWeather(weather.current));
-                }
-            });
+            navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
         }
     }, [])
-
-    
-
 
     return (
         <>
             {
-                geolocation
+                location
                     ?
-                    <StyledCurrentWeather>
+                    <StyledWelcome>
                         <div id='info'>
                             <div>
-                                <h1>{
-                                    isNaN(selectedPlace.name.split(',')[0])
-                                        ? selectedPlace.name.split(',')[0]
-                                        : selectedPlace.name.split(',')[1]
-                                }</h1>
-                                <p>{date.format('MMM, dddd YYYY')}</p>
+                                <h1>Your current weather is:</h1>
                             </div>
-                            <h3>{date.format('HH:mm')}</h3>
                         </div>
                         <div id='weather'>
                             <p>Today</p>
                             <div id='temperature'>
-                                <WeatherIcon id={id} icon={icon} size={100} />
+                                <WeatherIcon id={id} icon={icon} size={150} />
                                 <div>
                                     <h4><span>Feels-Like:</span> {kelvinToCelcius(feelsLike)}°</h4>
                                     <h5><span>Temperature:</span> {kelvinToCelcius(temp)}°</h5>
@@ -60,10 +62,22 @@ export const Welcome = () => {
                             </div>
                             <h5>{main}</h5>
                         </div>
-                    </StyledCurrentWeather >
-                    :
-                    <>
-                    </>
+                        <div id="date">
+                            <p>{date.format('MMM, dddd YYYY')}</p>
+                            <h3>{date.format('HH:mm')}</h3>
+                        </div>
+                    </StyledWelcome >
+                    : error
+                        ?
+                        <StyledMessage>
+                            <WeatherIcon id={200} icon='n' size={200} />
+                            <h1>There was a problem getting your curren position but don't worry, you can search for a place to see its weather.</h1>
+                        </StyledMessage>
+                        :
+                        <StyledMessage>
+                            <img src={locationIcon} />
+                            <h1>Please, let us have access to your current possition in order to show you your current weather.</h1>
+                        </StyledMessage>
             }
         </>
     )
